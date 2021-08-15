a70bb60a181a40a43382592e4c6a5008880512fc
82Daniela <danielandraderock@gmail.com>
2021-08-15 14:42:24 -0300

    Creating the entity classes

---
+++ src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -0,0 +1,15 @@
+package com.packt.cardatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardatabaseApplication {
+
+	//um comentário
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardatabaseApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/packt/domain/Car.java
@@ -0,0 +1,86 @@
+package com.packt.domain;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name="car")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    @Column(nullable = false)
+    private String brand;
+    @Column(nullable = false)
+    private String model;
+    @Column(nullable = false)
+    private String color;
+    @Column(nullable = false)
+    private String registerNumber;
+    @Column(nullable = false)
+    private int year;
+    @Column(nullable = false)
+    private  int price;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getRegisterNumber() {
+        return registerNumber;
+    }
+
+    public void setRegisterNumber(String registerNumber) {
+        this.registerNumber = registerNumber;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Car)) return false;
+        Car car = (Car) o;
+        return id.equals(car.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
